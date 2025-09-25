a713796fd269967a098cfdc9cb2e6bf59b92164f
RonaldoSC14 <145072482+RonaldoSC14@users.noreply.github.com>
2025-09-25 11:49:24 -0700

    Enemy and PowerUps
    Added an enemy and powerups to the project and added the scripts and materials for the objects player and enemy.

--- P3Serna1PlayMechanics/Assets/Scripts/PlayerController.cs
+++ P3Serna1PlayMechanics/Assets/Scripts/PlayerController.cs
@@ -3,19 +3,31 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
+    private GameObject focalPoint;
     public float speed = 5.0f;
+    public bool hasPowerup = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("Focal Point");
     }
 
     // Update is called once per frame
     void Update()
     {
-        float fowardInput = Input.GetAxis("Verical");
+        float forwardInput = Input.GetAxis("Vertical");
 
-        playerRb.AddForce(Vector3.forward * fowardInput * speed * fowardInput);
+        playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("PowerUp"))
+        {
+            hasPowerup = true;
+            Destroy(other.gameObject);
+        }
     }
 }
